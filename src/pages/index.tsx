504eb56d2787321e7e526dbb4fac8ed04c402f0a
Michael Mathias <mickmath86@gmail.com>
2023-01-17 00:32:37 -0800

    fix again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Script from 'next/script'
-import Crossword from './Crossword'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
